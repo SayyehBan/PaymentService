fd6b0cd1fba9f66c4048e2327c97b8b063118fd8
SayyehBan <sdvp1991david@gmail.com>
2024-04-22 01:02:43 +0330

    دریافت پیام اطلاعات پرداخت

--- PaymentService.EndPoint/Program.cs
+++ PaymentService.EndPoint/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using PaymentService.Application.Contexts;
+using PaymentService.Application.Services;
+using PaymentService.Infrastructure.MessagingBus.ReceivedMessage.GetPaymetMessages;
 using PaymentService.Persistence.Context;
+using SayyehBanTools.ConfigureService;
 using SayyehBanTools.ConnectionDB;
+using SayyehBanTools.MessagingBus.RabbitMQ.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +14,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddTransient<IPaymentDataBaseContext, PaymentDataBaseContext>();
 builder.Services.AddDbContext<PaymentDataBaseContext>(p =>
-p.UseSqlServer(SqlServerConnection.ConnectionString("urMcjpPHmP8SS3v70kndBw==", "haUa5gTQBP9rckJh9Za4/A==", "QbLwKXetr0dBjiUhNUNpvQ==", "70D4ul+uXR+y357iQaTIJw==", "5m6h7m9i9386b9c6", "axoz52ew9510gnmc")));
+p.UseSqlServer(SqlServerConnection.ConnectionString("urMcjpPHmP8SS3v70kndBw==", "haUa5gTQBP9rckJh9Za4/A==", "QbLwKXetr0dBjiUhNUNpvQ==", "70D4ul+uXR+y357iQaTIJw==", "5m6h7m9i9386b9c6", "axoz52ew9510gnmc")),ServiceLifetime.Singleton); 
+var configureServices = new ConfigureServicesRabbitMQ();
+configureServices.ConfigureService(builder.Services);
+//RabbitMQ
+builder.Services.Configure<RabbitMqConnectionSettings>(builder.Configuration
+    .GetSection("RabbitMq"));
+builder.Services.AddTransient<IPaymentService, RPaymentServiceConcrete>();
+builder.Services.AddHostedService<RecievedMessagePaymentForOrder>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ PaymentService.Infrastructure/MessagingBus/ReceivedMessage/GetPaymetMessages/RecievedMessagePaymentForOrder.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using PaymentService.Application.Services;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using SayyehBanTools.MessagingBus.RabbitMQ.Connection;
+using SayyehBanTools.MessagingBus.RabbitMQ.Model;
+using System.Text;
+
+namespace PaymentService.Infrastructure.MessagingBus.ReceivedMessage.GetPaymetMessages;
+
+public class RecievedMessagePaymentForOrder : BackgroundService
+{
+    private readonly RabbitMqConnectionSettings _rabbitMqConnectionSettings;
+    private readonly RabbitMQConnection rabbitMQConnection;
+    private readonly string _queueName;
+    private readonly IPaymentService paymentService;
+    public RecievedMessagePaymentForOrder(IPaymentService paymentService,RabbitMQConnection rabbitMQConnection, IOptions<RabbitMqConnectionSettings> rabbitMqConnectionSettings)
+    {
+        this.paymentService = paymentService;
+        _rabbitMqConnectionSettings = rabbitMqConnectionSettings.Value;
+        _queueName = _rabbitMqConnectionSettings.queue;
+        this.rabbitMQConnection = rabbitMQConnection;
+        this.rabbitMQConnection.CreateRabbitMQConnection();
+        this.rabbitMQConnection.Channel = rabbitMQConnection.Connection.CreateModel();
+        this.rabbitMQConnection.Channel.QueueDeclare(queue: _queueName, durable: true,
+            exclusive: false, autoDelete: false, arguments: null);
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var consumer = new EventingBasicConsumer(rabbitMQConnection.Channel);
+        consumer.Received += (ch, ea) =>
+        {
+            var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+            var message = JsonConvert
+            .DeserializeObject<MessagePaymentDto>(content);
+            var resultHandeleMessage =
+            HandleMessage(message.OrderId, message.Amount);
+
+            if (resultHandeleMessage)
+                rabbitMQConnection.Channel.BasicAck(ea.DeliveryTag, false);
+        };
+        rabbitMQConnection.Channel.BasicConsume(queue: _queueName, false, consumer);
+        return Task.CompletedTask;
+    }
+    private bool HandleMessage(Guid OrderId, int Amount)
+    {
+        return paymentService.CreatePayment(OrderId, Amount);
+    }
+}
+public class MessagePaymentDto
+{
+    public Guid OrderId { get; set; }
+    public int Amount { get; set; }
+}
